dc8bef12b75e9b1ed03b294fbff74cb653912277
Vignesh Manikandan <vigneshmanikandan98@gmail.com>
2024-02-01 22:32:29 -0600

    Updates

---
+++ precache-manifest.b2f964335060ef9c350810e951aea6b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da713abecab64e3665516728a44698b9",
+    "url": "/new-personal/index.html"
+  },
+  {
+    "revision": "3c8f25c5590acf7b99af",
+    "url": "/new-personal/static/css/main.2f402fd6.chunk.css"
+  },
+  {
+    "revision": "f423be8708a7e4f319b9",
+    "url": "/new-personal/static/js/2.1793750c.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/new-personal/static/js/2.1793750c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6ff3f4fcc4492c3d4c",
+    "url": "/new-personal/static/js/3.b2feef38.chunk.js"
+  },
+  {
+    "revision": "3c8f25c5590acf7b99af",
+    "url": "/new-personal/static/js/main.3bbd57e4.chunk.js"
+  },
+  {
+    "revision": "40b5acd6fd53178a3c89",
+    "url": "/new-personal/static/js/runtime-main.07ea86d6.js"
+  }
+]);
