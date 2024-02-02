220175c82b39403bc38f8113b734aa615a91785e
Vignesh Manikandan <vigneshmanikandan98@gmail.com>
2024-02-01 22:43:32 -0600

    Deployed website

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/new-personal/precache-manifest.b2f964335060ef9c350810e951aea6b2.js"
+  "/new-personal/precache-manifest.5078cb37ccb27922c8a2b0afdd3bdcc1.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/Ask.js
+++ src/Ask.js
@@ -3,9 +3,9 @@ import React, { useState } from 'react';
 
 
 function Ask() {
-    const question = "/images/ask.gif";
-    const yes = "/images/yes.gif";
-    const sad = "/images/sad-sad-cat.gif"
+    const question = "./images/ask.gif";
+    const yes = "./images/yes.gif";
+    const sad = "./images/sad-sad-cat.gif"
     const red = "#cf1735"
     const white = "white"
 
@@ -30,7 +30,7 @@ function Ask() {
                 <br/>
                 <div className='buttons'>
                     {
-                        hb ? (<img style={{height: '100px'}} src='images/heartbreak.jpg'></img>) : (
+                        hb ? (<img style={{height: '100px'}} src='./images/heartbreak.jpg'></img>) : (
                             <>
                             <button onClick={handleYes} className='sub' style={{backgroundColor: 'whitesmoke', width: '30%', margin: '20px 20px'}} >Yes</button>                    
                             <button onClick={handleNo} className='sub' style={{backgroundColor: 'whitesmoke', width: '30%', margin: '20px 20px'}} >No</button>
